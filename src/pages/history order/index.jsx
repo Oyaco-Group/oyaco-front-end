dead0ee5e98c5825f85731ece152b2b4969b4799
syahril <syahrill.ramadhan02@gmail.com>
2024-07-16 22:08:27 +0700

    Re-push file history order and product list page

--- src/pages/history order/index.jsx
+++ src/pages/history order/index.jsx
@@ -3,7 +3,7 @@ const HistoryOrderPage = () => {
     <div>
       <div className="p-4 sm:ml-64">
         <div className="mt-14 rounded-lg border-2 border-dashed border-gray-200 p-4 dark:border-gray-700">
-          <h1 className="text-2xl"> History Order Page</h1>
+          <h1 className="text-2xl">History Order Page</h1>
         </div>
       </div>
     </div>
--- src/pages/product list/index.jsx
+++ src/pages/product list/index.jsx
@@ -3,7 +3,7 @@ const ProductListPage = () => {
     <div>
       <div className="p-4 sm:ml-64">
         <div className="mt-14 rounded-lg border-2 border-dashed border-gray-200 p-4 dark:border-gray-700">
-          <h1 className="text-2xl"> Product List Page</h1>
+          <h1 className="text-2xl"> Product List</h1>
         </div>
       </div>
     </div>
