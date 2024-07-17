d5ace6d442977526ecdca8bf28e1f28687c4fab3
syahril <syahrill.ramadhan02@gmail.com>
2024-07-17 18:08:57 +0700

    fix: changes tag Image to img fix issue

--- src/components/style-components/auth/auth-image.jsx
+++ src/components/style-components/auth/auth-image.jsx
@@ -1,22 +1,14 @@
-import Image from "next/image";
-
 const AuthImage = () => {
   return (
     <div className="flex h-screen w-1/2 flex-col items-center justify-center">
       <div className="mt-[-150px] h-40">
-        <Image src="/logo.png" alt="Logo" width={500} height={100} priority />
+        <img src="/logo.png" alt="Logo" width={500} height={100} />
       </div>
       <p className="mt-4 tracking-[1px] text-sky-300">
         Efficiently manage your product stock with ease
       </p>
       <div className="mt-10">
-        <Image
-          src="/avatar.png"
-          alt="Avatar"
-          width={250}
-          height={250}
-          priority
-        />
+        <img src="/avatar.png" alt="Avatar" width={250} height={250} />
       </div>
     </div>
   );
