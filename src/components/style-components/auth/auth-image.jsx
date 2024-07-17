c4d2814c9e3e989d67c5533ff9a363464047d94e
syahril <syahrill.ramadhan02@gmail.com>
2024-07-17 18:02:55 +0700

    Fix: add priority for LCP

--- src/components/style-components/auth/auth-image.jsx
+++ src/components/style-components/auth/auth-image.jsx
@@ -4,13 +4,19 @@ const AuthImage = () => {
   return (
     <div className="flex h-screen w-1/2 flex-col items-center justify-center">
       <div className="mt-[-150px] h-40">
-        <Image src="/logo.png" alt="Logo" width={500} height={100} />
+        <Image src="/logo.png" alt="Logo" width={500} height={100} priority />
       </div>
       <p className="mt-4 tracking-[1px] text-sky-300">
         Efficiently manage your product stock with ease
       </p>
       <div className="mt-10">
-        <Image src="/avatar.png" alt="Avatar" width={250} height={250} />
+        <Image
+          src="/avatar.png"
+          alt="Avatar"
+          width={250}
+          height={250}
+          priority
+        />
       </div>
     </div>
   );
