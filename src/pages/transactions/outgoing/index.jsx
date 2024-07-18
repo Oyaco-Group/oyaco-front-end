f05e631c9932b6c05e8f692a143ebfd9252482f7
adolfalmeida <adolfalmeidas@gmail.com>
2024-07-18 14:39:21 +0700

    Outgoing Transaction features

--- src/pages/transactions/outgoing/index.jsx
+++ src/pages/transactions/outgoing/index.jsx
@@ -1,13 +1,57 @@
-const TransactionOutoingPage = () => {
+import React, { useState, useEffect } from "react";
+import Table from "@/components/style-components/table";
+import Dropdown from "@/components/style-components/dropdown";
+import SpinnerLoad from "@/components/style-components/loading-indicator/spinner-load";
+import SearchBar from "@/components/style-components/navbar/searchbar";
+import Button from "@/components/style-components/button";
+
+const TransactionOutgoingPage = () => {
+  const columns = [
+    { field: "id", label: "ID" },
+    { field: "user_id", label: "Admin ID" },
+    { field: "master_product_id", label: "Product ID" },
+    { field: "inventory_id", label: "Inventory ID" },
+    { field: "destination", label: "Deliverd to" },
+    { field: "quantity", label: "Quantity" },
+    { field: "arrival_date", label: "Date" },
+    { field: "expiration_date", label: "Expiration Date" },
+    { field: "action", label: "Action" },
+  ];
+
   return (
     <div>
       <div className="p-4 sm:ml-64">
-        <div className="mt-14 rounded-lg border-2 border-dashed border-gray-200 p-4 dark:border-gray-700">
-          <h1 className="text-2xl"> Transaksi outgoing</h1>
+        <div className="mt-14 rounded-lg p-4 dark:border-gray-700">
+          <h1 className="mt-4 mb-6 text-2xl text-gray-800">
+            {" "}
+            Outgoing Transaction{" "}
+          </h1>
+          <div className="relative overflow-x-auto">
+            <div className="flex flex-wrap items-center justify-between space-y-4 bg-white py-4 md:flex-row md:space-y-0 dark:bg-gray-900">
+              <div className="flex items-center gap-4">
+                <p>Select warehouse: </p>
+                <Dropdown />
+
+                <Dropdown />
+              </div>
+              <div>
+                <SearchBar className="w-72" />
+              </div>
+              <div>
+                <Button> Create Transaction </Button>
+              </div>
+            </div>
+            <div className="flex items-center justify-center">
+              {/* {isLoading && <SpinnerLoad />} */}
+            </div>
+            {/* {!isLoading && (
+            <Table columns={a} data={a} onEdit={a} />
+          )} */}
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default TransactionOutoingPage;
+export default TransactionOutgoingPage;
