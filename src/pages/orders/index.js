03df01ee7931001facdf060a8cdfa0019246e753
syahril <syahrill.ramadhan02@gmail.com>
2024-07-19 00:19:14 +0700

    Refac: Refactory rename file jsx to jsx

---
+++ src/pages/orders/index.js
@@ -0,0 +1,71 @@
+import TableOrder from "@/components/style-components/TableOrder";
+import { getAllOrder } from "@/fetching/order";
+import { useEffect, useState } from "react";
+import SearchBar from "@/components/style-components/navbar/searchbar";
+import Dropdown from "@/components/style-components/dropdown";
+import Button from "@/components/style-components/button";
+import Modal from "@/components/style-components/modal";
+import DetailOrder from "./detail";
+
+const OrderPage = () => {
+  const [order, setOrder] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const fetchOrder = async () => {
+    try {
+      const data = await getAllOrder();
+      setOrder(data.data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  function openModal() {
+    setIsOpen(true);
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  useEffect(() => {
+    fetchOrder();
+    setLoading(true);
+  }, [order, isOpen]);
+
+  const columns = [
+    // {label : 'No', field : "no"},
+    { label: "Order Id ", field: "id" },
+    { label: "Date", field: "created_at" },
+    { label: "Order Status", field: "order_status" },
+    { label: "Order Type", field: "buyer_status" },
+    { label: "Payment Type", field: "payment_type" },
+    { label: "Action", field: "action" },
+  ];
+
+  return (
+    <div className="p-4 sm:ml-64">
+      <div className="mt-14 rounded-lg p-4 dark:border-gray-700">
+        <h1 className="mt-4 text-2xl text-gray-800">Order List</h1>
+        <p className="mb-6 text-sm font-light text-gray-400">
+          Manage Order List
+        </p>
+        <div className="relative overflow-x-auto">
+          <div className="flex flex-wrap items-center justify-around space-y-4 bg-white py-4 md:flex-row md:space-y-0 dark:bg-gray-900">
+            <Dropdown />
+
+            <button className="mb-2 me-2 rounded-lg bg-gradient-to-r from-green-400 via-green-500 to-green-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-green-300 dark:focus:ring-green-800">
+              Add Order
+            </button>
+          </div>
+        </div>
+        <TableOrder columns={columns} data={order} onEdit={openModal} />
+      </div>
+
+      <DetailOrder onClose={closeModal} isOpen={isOpen} />
+    </div>
+  );
+};
+
+export default OrderPage;
