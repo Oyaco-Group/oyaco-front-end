7a219e272e7cbaea005a1e71793550f04cfcb5e0
syahril <syahrill.ramadhan02@gmail.com>
2024-07-19 06:53:36 +0700

    fix : issue invalid component count-card and navbar

--- src/components/style-components/card-count.js
+++ src/components/style-components/card-count.js
@@ -4,19 +4,7 @@ const CardCount = ({ icon, count, label }) => {
   return (
     <div className="w-full p-4 sm:w-1/2 md:w-1/4">
       <div className="transform rounded-lg border-2 border-gray-200 px-4 py-6 shadow-md transition duration-500 hover:scale-110">
-        {icon && (
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            className="mb-3 inline-block h-12 w-12 text-blue-400"
-            viewBox="0 0 24 24"
-          >
-            {icon}
-          </svg>
-        )}
+        {icon}
         <h2 className="title-font text-3xl font-medium text-gray-900">
           {count}
         </h2>
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -9,24 +9,24 @@ const DashboardPage = () => {
           <div className="body-font container mx-auto my-14 text-gray-700">
             <div className="-m-4 flex flex-wrap text-center">
               <CardCount
-                icon={<path d="M8 17l4 4 4-4m-4-5v9"></path>}
+                // icon={<path d="M8 17l4 4 4-4m-4-5v9"></path>}
                 count={500}
                 label="Product"
               />
               <CardCount
-                icon={<path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>}
+                // icon={<path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>}
                 count={100}
                 label="Users"
               />
               <CardCount
-                icon={<path d="M3 18v-6a9 9 0 0118 0v6"></path>}
+                // icon={<path d="M3 18v-6a9 9 0 0118 0v6"></path>}
                 count={74}
                 label="Order"
               />
               <CardCount
-                icon={
-                  <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-                }
+                // icon={
+                //   <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                // }
                 count={46}
                 label="Complain"
               />
