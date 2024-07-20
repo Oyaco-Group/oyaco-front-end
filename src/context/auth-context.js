9c9650f1b18b8ca110f1caf456c05ae9823be1b8
syahril <syahrill.ramadhan02@gmail.com>
2024-07-20 12:06:08 +0700

    Perf: add link create a new account

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -6,7 +6,6 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    // Check local storage for existing token and user data on initial load
     const token = localStorage.getItem("token");
     const userData = localStorage.getItem("user");
     if (token && userData) {
