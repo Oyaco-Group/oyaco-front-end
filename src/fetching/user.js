ca86b6d5e30ae55ba16550d3b3de8693c52098a0
syahril <syahrill.ramadhan02@gmail.com>
2024-07-21 15:46:55 +0700

    perf: Fetching user data

--- src/fetching/user.js
+++ src/fetching/user.js
@@ -1,11 +1,19 @@
 import instance from "@/lib/axios";
 
-export const fetchUsers = async () => {
+export const fetchUsers = async (page = 1, limit = 5) => {
   try {
-    const response = await instance.get("/categories");
-    return response.data.data;
+    const response = await instance.get(`/user`, {
+      params: {
+        page: page,
+        limit: limit,
+      },
+    });
+    return response.data;
   } catch (error) {
-    console.error("Error fetching category data:", error);
+    console.error(
+      "Error fetching user data:",
+      error.response || error.message || error
+    );
     throw error;
   }
 };
