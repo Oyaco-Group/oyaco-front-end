62a600b468253f33b343f94114b978ee6451847e
adolfalmeida <adolfalmeidas@gmail.com>
2024-07-25 22:07:33 +0700

    commit 2

---
+++ src/fetching/incomingTransaction.js
@@ -0,0 +1,39 @@
+import instance from "@/lib/axios";
+
+const getAllIncomingTransactions = async (warehouse, page) => {
+  const response = await instance({
+    method: "GET",
+    url: `/transactions/incoming/${warehouse}?page=${page}`,
+  });
+  return response.data;
+};
+
+const getWarehouses = async () => {
+  const response = await instance({
+    method: "GET",
+    url: `/warehouses`,
+  });
+  return response.data.data;
+};
+
+const createTransaction = async (transactionData) => {
+  const response = await instance({
+    method: "POST",
+    url: `/transactions`,
+    data: transactionData,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return response.data.data;
+};
+
+// const getUser = async (page, limit) => {
+//   const response = await instance({
+//     method: "GET",
+//     url: `/user?page=${page}&limit=${limit}`,
+//   });
+//   return response.data.data;
+// };
+
+export { getAllIncomingTransactions, getWarehouses, createTransaction };
