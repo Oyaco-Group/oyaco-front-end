51ab3a449c5002633385c83ea6f3c4699521f8d4
adolfalmeida <143357238+adolfalmeida@users.noreply.github.com>
2024-07-26 17:04:20 +0700

    Feature/inventory (#49)
    * Outgoing Transaction features
    
    * added some transaction feature
    
    * added get by warehouse
    
    * conflict resolved
    
    * added some feature
    
    * commit feature/outgoingTransaction
    
    * commit
    
    * commit 2
    
    * outgoing almost finished
    
    * first inventory commit
    
    * inventory updated

--- src/fetching/inventory.js
+++ src/fetching/inventory.js
@@ -1,15 +1,28 @@
-import instance from '@/lib/axios';
+import instance from "@/lib/axios";
 
+const getAllStock = async (page) => {
+  const response = await instance({
+    method: "GET",
+    url: `/inventory/stock?page=${page}`,
+  });
+  return response.data.data;
+};
 
-const getInventoryByProductId = async(id) => {
+const getStockByWarehouse = async (warehouse_id, page) => {
     const response = await instance({
-        method : 'GET',
-        url : `/inventory/product/${id}`
-    })
+      method: "GET",
+      url: `/inventory/warestock/${warehouse_id}?page=${page}`,
+    });
     return response.data.data;
-}
+  };
 
+  const getWarehouses = async () => {
+    const response = await instance({
+      method: "GET",
+      url: `/warehouses`,
+    });
+    return response.data.data;
+  };
+  
 
-export {
-    getInventoryByProductId
-}
+export { getAllStock, getStockByWarehouse, getWarehouses };
