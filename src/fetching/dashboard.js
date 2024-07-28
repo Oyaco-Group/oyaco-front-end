23715ea3495a5041329aacb1daca198344a3f39b
syahril <syahrill.ramadhan02@gmail.com>
2024-07-28 22:04:22 +0700

    perf: integration api dashboard and fix layout

---
+++ src/fetching/dashboard.js
@@ -0,0 +1,14 @@
+import instance from "@/lib/axios";
+
+export const fetchDashboard = async () => {
+  try {
+    const response = await instance.get("/dashboard");
+    if (response.status !== 200) {
+      throw new Error("Failed to fetch dashboard data");
+    }
+    return response.data.data;
+  } catch (error) {
+    console.error("Error in fetchDashboard:", error);
+    throw error;
+  }
+};
