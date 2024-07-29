18986bd4ac399e5d8ee67d4e6691000b9d50a0e1
syahril <syahrill.ramadhan02@gmail.com>
2024-07-30 00:09:51 +0700

    Del: remove console.log

--- src/fetching/dashboard.js
+++ src/fetching/dashboard.js
@@ -3,7 +3,6 @@ import instance from "@/lib/axios";
 export const fetchDashboard = async () => {
   try {
     const response = await instance.get("/dashboard");
-    console.log(response);
     if (response.status !== 200) {
       throw new Error("Failed to fetch dashboard data");
     }
