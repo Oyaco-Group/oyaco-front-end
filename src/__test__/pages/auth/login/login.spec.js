798869d2c70925aa23b5129d51d1ebe2acc74639
syahril <syahrill.ramadhan02@gmail.com>
2024-08-01 00:51:24 +0700

    set up unit testing jest

--- src/__test__/pages/auth/login/login.spec.js
+++ src/__test__/pages/auth/login/login.spec.js
@@ -1,7 +1,9 @@
 import { render } from "@testing-library/react";
 import LoginPage from "@/pages/login/index";
 
+//Scenario
 describe("Login page", () => {
+  //Test case
   it("renders login page correctly", () => {
     const { page } = render(<LoginPage />);
     expect(page).toMatchSnapshot();
