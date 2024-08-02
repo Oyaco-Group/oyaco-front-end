32266a70f114be6aed8a60579e0de384edf87be3
sufandijkPhy <112595174+mochammadsufandi@users.noreply.github.com>
2024-08-02 21:51:55 +0700

    Feature/order (#81)
    * fix : add overflow and max-h class in modal, add onChange props in TableOrder, fix styling order
    
    * fix : error after pull and merge
    
    * fix : image src in profile-menu, edit user, pagination in masterProduct, fix sendOrder
    
    * fix : error double function
    
    * fix : product (using filtered data if search data), styling order, styling create order

--- src/pages/orders/index.js
+++ src/pages/orders/index.js
@@ -14,10 +14,12 @@ import Pagination from "@/components/style-components/pagination";
 import ChangeStatus from "./changeStatus";
 import SendOrder from "./sendOrder";
 import { useAuth } from "@/context/authContext";
+import { FaPlus } from "react-icons/fa6";
 
 const OrderPage = () => {
   const { user } = useAuth();
   const [order, setOrder] = useState([]);
+  const [filteredOrder, setFilteredOrder] = useState([]);
   const [isOpen1, setisOpen1] = useState(false);
   const [isOpen2, setisOpen2] = useState(false);
   const [isOpen3, setisOpen3] = useState(false);
@@ -26,12 +28,14 @@ const OrderPage = () => {
   const [totalPages, setTotalPages] = useState(0);
   const [id, setId] = useState();
   const [detailOrder, setDetailOrder] = useState({});
+  const [searchOrder, setSearchOrder] = useState("");
   const router = useRouter();
 
   const fetchOrder = async () => {
     try {
       const data = await getAllOrder(page, limit);
       setOrder(data.data);
+      setFilteredOrder(data.data);
       setTotalPages(data.metadata.totalPages);
     } catch (err) {
       console.log(err);
@@ -45,6 +49,21 @@ const OrderPage = () => {
     } catch (err) {
       console.log(err);
     }
+  };
+
+  const filterOrder = (valueSearch) => {
+    let filteredOrder = order;
+
+    if (valueSearch) {
+      filteredOrder = filteredOrder.filter(
+        (order) =>
+          order.order_status.toLowerCase().includes(valueSearch.toLowerCase()) ||
+          order.buyer_status.toLowerCase().includes(valueSearch.toLowerCase()) ||
+          order.payment_type.toLowerCase().includes(valueSearch.toLowerCase()) ||
+          order.created_at.toLowerCase().includes(valueSearch.toLowerCase())
+      );
+    }
+    setFilteredOrder(filteredOrder);
   };
 
   function openModal1(id) {
@@ -79,18 +98,24 @@ const OrderPage = () => {
 
   const sendOrderHandler = async (params) => {
     try {
-      const { id } = params;
-      const admin_id = user.id;
-      // console.log(params, admin_id);
-      const order = await sendOrder(id, params, admin_id);
+      const {id} = params;
+      const admin_id = user.id;;
+      const order = await sendOrder(id,params,admin_id);
       setisOpen2(false);
       toast.success(order.message);
+      
     } catch (err) {
       console.log(err.message);
       toast.error(err.response.data.message);
     }
   };
 
+  const handleSearchChange = (event) => {
+    const value = event.target.value;
+    setSearchOrder(value);
+    filterOrder(value);
+  };
+  
   const handlePageChange = (newPage) => {
     setPage(newPage);
   };
@@ -127,22 +152,38 @@ const OrderPage = () => {
   ];
 
   return (
-    <div className="p-4 sm:ml-64">
-      <div className="mt-14 rounded-lg p-4 dark:border-gray-700">
-        <h1 className="mt-4 text-2xl text-gray-800">Order List</h1>
-        <p className="mb-1 text-sm font-light text-gray-400">
-          Manage Order List
-        </p>
-        <div className="relative overflow-x-auto">
-          <div className="flex flex-wrap items-center justify-end space-y-4 bg-white py-3 md:flex-row md:space-y-0 dark:bg-gray-900">
-            <Button
-              className="bg-green-500 hover:bg-green-600"
-              onClick={() => {
-                router.push("/orders/create");
-              }}
-            >
-              Add Order
-            </Button>
+      <div className="p-4 sm:ml-64">
+        <div className="mt-14 rounded-lg p-4 dark:border-gray-700">
+            <h1 className="mt-4 text-2xl text-gray-800">Order List</h1>
+            <p className="mb-1 text-sm font-light text-gray-400">
+              Manage Order List
+            </p>
+            <div className="relative overflow-x-auto">
+                <div className="flex flex-wrap items-center justify-between space-y-4 bg-white py-3 md:flex-row md:space-y-0 dark:bg-gray-900">
+                  <div>
+                    <SearchBar
+                      className="w-72"
+                      onChange={handleSearchChange}
+                      value={searchOrder}
+                    />
+                  </div>
+                  <Button className="bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-0 flex gap-2 items-center justify-between"
+                    onClick={() => {
+                      router.push('/orders/create');
+                    }}>
+                      <FaPlus/>
+                      Add Order
+                  </Button>
+                </div>
+                <TableOrder 
+                    columns={columns} 
+                    data={filteredOrder} 
+                    onDetail={openModal1} 
+                    onChange={openModal3}
+                    onEdit={onEdit}
+                    sendOrder={openModal2}
+                />
+            </div>
           </div>
           <TableOrder
             columns={columns}
